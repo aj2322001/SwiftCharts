1b72ae9e1fcfaae88e5e8273ac4d4e507ac46be6
Archit Joshi <archit.joshi@anarock.com>
2025-01-14 01:41:10 +0530

    Initial Commit

---
+++ SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Archit Joshi on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
